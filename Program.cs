6cc9ea84b6db40b5c59b795c3e0c67f43aa0df3e
VitoriaRossi <vitoria.rossi555@gmail.com>
2023-08-22 16:55:25 -0300

    Adiciona a resposta do exercício

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Dó,"); Console.Beep(1320,200);
+Console.WriteLine("ré,"); Console.Beep(1485,200);
+Console.WriteLine("mi,"); Console.Beep(1650,200);
+Console.WriteLine("fá,"); Console.Beep(1759,200);
+Thread.Sleep(500);
+Console.WriteLine("fá,"); Console.Beep(1759,200);
+Console.WriteLine("fá,"); Console.Beep(1759,200);
+Thread.Sleep(1000);
+
+Console.WriteLine();
+
+Console.WriteLine("Dó,"); Console.Beep(1320,200);
+Console.WriteLine("ré,"); Console.Beep(1485,200);
+Console.WriteLine("dó,"); Console.Beep(1320,200);
+Console.WriteLine("ré,"); Console.Beep(1485,200);
+Thread.Sleep(500);
+Console.WriteLine("ré,"); Console.Beep(1485,200);
+Console.WriteLine("ré,"); Console.Beep(1485,200);
+Thread.Sleep(1000);
+
+Console.WriteLine();
+
+Console.WriteLine("Dó,"); Console.Beep(1320,200);
+Console.WriteLine("sol,"); Console.Beep(1980,200);
+Console.WriteLine("fá,"); Console.Beep(1759,200);
+Console.WriteLine("mi,"); Console.Beep(1650,200);
+Thread.Sleep(500);
+Console.WriteLine("mi,"); Console.Beep(1650,200);
+Console.WriteLine("mi,"); Console.Beep(1650,200);
+Thread.Sleep(1000);
+
+Console.WriteLine();
+
+Console.WriteLine("Dó,"); Console.Beep(1320,200);
+Console.WriteLine("ré,"); Console.Beep(1485,200);
+Console.WriteLine("mi,"); Console.Beep(1650,200);
+Console.WriteLine("fá,"); Console.Beep(1759,200);
+Thread.Sleep(500);
+Console.WriteLine("fá,"); Console.Beep(1759,200);
+Console.WriteLine("fá,"); Console.Beep(1759,200);
+Thread.Sleep(1000);
+
+
+
